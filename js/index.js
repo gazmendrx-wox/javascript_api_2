8da70ac1e1718905433d72aadb76254d4c177258
Gazmendrx <gazmend@wox-llc.com>
2024-02-11 21:23:01 +0100

    update

--- js/index.js
+++ js/index.js
@@ -14,24 +14,38 @@
 document.addEventListener("DOMContentLoaded", function(){
     //
     function handleLoginForm(email, password) {
-        console.log({email, password})
-
         /**
          * atuny0@sohu.com / 9uQFF1Lh
          * kmeus4@upenn.edu / aUTdmmmbH
          */
 
-        fetch('https://dummyjson.com/users')
-        .then(res => res.json())
-        .then((data) => {
-            //filter data - by email and password
+        fetch('https://dummyjson.com/users') //return me all users;
+        .then(res => res.json()) //parse it in json
+        .then((data) => { //function - let me decide what to do with that response
+            const allUsers = data.users;
+            //find data - by email and password
+            const user = allUsers.find((user) => user.email === email && user.password === password)
 
             //if found - hide login form - show dashbaord
+            if(user) {
+                //
+            }
 
             //if not found - show error message
         });
     }
 
-    handleLoginForm('atuny0@sohu.com', '9uQFF1Lh')
+    /**
+     * button per login??
+     */
+    document.getElementById('button').addEventListener('click', function() {
+
+        //email, edhe password, edhe pe percjellmi te
+        //te function handleLoginForm(email,password);
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+
+        handleLoginForm(email, password);
+    })
 
 })
